5baa3fac846d09f80f29b3ae2bb4ffbf698ec027
Washington Vieira <wv817816@gmail.com>
2024-10-03 11:43:40 -0300

    Query_Cliente_Sig correta

--- Query_Cliente_Sig_correta.sql
+++ Query_Cliente_Sig_correta.sql
@@ -1,27 +1,24 @@
-with precos_medios as (
-  select
+WITH precos_medios AS (
+  SELECT
     cod_empresa,
     cod_produto,
     cod_deposito,
     data_movimento,
-	   
-    /* É esperado que o preço médio seja único por depósito e data. Mas usamos a agregação para não ter que colocar o campo no GROUP BY, 
-       pois poderia gerar divisão nos registros caso encontrasse, por algum erro no sistema, mais de um preço médio e isso causaria 
-       sumarizações erradas na query principal por causa de lançamentos duplicados resultados no JOIN com precos_medios*/
-    CAST(MAX(COALESCE(preco_medio,0)) AS numeric(18,8)) AS preco_medio,
-	
-    tipo_lancamento,	
-    SUM(CASE WHEN tipo_lancamento = 'V' THEN valor 
-             ELSE -valor END) AS total_valor_vendas,
-    SUM(tributos_sobre_vendas) AS total_tributos_sobre_vendas --Para as devoluções os tributos sobre vendas já estão negativos e são subtraídos no somatório.                                     
-  from
+    
+    -- Preço médio por depósito e data
+    CAST(MAX(COALESCE(preco_medio, 0)) AS numeric(18,8)) AS preco_medio,
+    
+    tipo_lancamento,
+    SUM(CASE WHEN tipo_lancamento = 'V' THEN valor ELSE -valor END) AS total_valor_vendas,
+    SUM(tributos_sobre_vendas) AS total_tributos_sobre_vendas -- Para as devoluções, os tributos sobre vendas já estão negativos
+  FROM
     relatorios.movimentacao_produtos_com_lucro_bruto_medio(2, null, 0, null, '01/10/2024', current_date)
   WHERE
-    tipo_lancamento IN ('V', 'X')                          
+    tipo_lancamento IN ('V', 'X')
   GROUP BY
     cod_empresa,
     cod_produto,
-    data_movimento,              
+    data_movimento,
     cod_deposito,
     tipo_lancamento
 )
@@ -40,59 +37,53 @@ SELECT
   produto,
   preco,
   preco_medio,
-  sum(CASE WHEN tipo_lancamento = 'X' THEN -quantidade 
-           ELSE quantidade END) as total_quantidade,
-  sum(CASE WHEN tipo_lancamento = 'X' THEN -custo
-           ELSE custo END) as custo_total,
-  sum(CASE WHEN tipo_lancamento = 'X' THEN -valor
-           ELSE valor END) as total_valor,
-  sum(CASE WHEN tipo_lancamento = 'X' THEN -(valor - custo - tributos_sobre_venda)
-           ELSE (valor - custo - tributos_sobre_venda) END) as lucro_medio,
-  sum(CASE WHEN tipo_lancamento = 'X' THEN -tributos_sobre_venda
-           ELSE tributos_sobre_venda END) as total_tributos_sobre_venda
+  SUM(CASE WHEN tipo_lancamento = 'X' THEN -quantidade ELSE quantidade END) AS total_quantidade,
+  SUM(CASE WHEN tipo_lancamento = 'X' THEN -custo ELSE custo END) AS custo_total,
+  SUM(CASE WHEN tipo_lancamento = 'X' THEN -valor ELSE valor END) AS total_valor,
+  SUM(CASE WHEN tipo_lancamento = 'X' THEN -(valor - custo - tributos_sobre_venda) ELSE (valor - custo - tributos_sobre_venda) END) AS lucro_medio,
+  SUM(CASE WHEN tipo_lancamento = 'X' THEN -tributos_sobre_venda ELSE tributos_sobre_venda END) AS total_tributos_sobre_venda
 FROM
-  (select
-     lp.cod_empresa,
-     em.nome as empresa,
-     cl.id as id_classe,
-     cl.nome as classe,
-     lf.cod_pessoa AS cod_responsavel,
-     pe.nome_fantasia as responsavel,
-     gr.id as id_grupo,
-     gr.nome as grupo,
-     lp.cod_produto,
-     pr.nome as produto,
-     lp.preco,
-     pm.preco_medio,
-     lp.tipo_lancamento,
-     lp.data_movimento,
-     lp.quantidade,
-     CAST(lp.quantidade * pm.preco_medio AS numeric(14,2)) AS custo,
-     lp.valor,
-     CAST(lp.valor * (CASE WHEN total_valor_vendas <> 0 THEN (total_tributos_sobre_vendas/total_valor_vendas) ELSE 0 END) 
-	   AS numeric(14,2)) AS tributos_sobre_venda
-   from
-     lancamentos_produtos lp
-     inner join produtos pr ON (lp.cod_produto = pr.cod_produto)
-     left join precos_medios pm ON (lp.cod_empresa = pm.cod_empresa AND lp.cod_produto = pm.cod_produto AND 
-       lp.data_movimento = pm.data_movimento AND COALESCE(lp.cod_deposito,0) = COALESCE(pm.cod_deposito,0) AND --O depósito pode ser nulo no caso do serviços/receitas
-       lp.tipo_lancamento = pm.tipo_lancamento)
-     inner join grupos gr ON (pr.cod_grupo = gr.cod_grupo)
-     inner join lancamentos_financeiros lf ON (lp.cod_lanc_financeiro = lf.cod_lanc_financeiro)
-     inner join pessoas pe ON (lf.cod_pessoa = pe.cod_pessoa)
-     inner join classes cl ON (pe.cod_classe = cl.cod_classe)
-     inner join pessoas em on (em.cod_pessoa = lp.cod_empresa)
-   where
-     lp.cod_empresa = 2 and
-     lf.cod_pessoa = 43463 and
-     (lp.data_movimento between '01/10/2024' and current_date) and
-     (lp.situacao = 2) and
-     (not lp.cancelado) and
-     (lp.tipo_lancamento IN ('V', 'X'))
-     --(:p_cod_grupo = 0 or gr.id ilike :p_id_grupo || '%') and
-    -- (:p_cod_classe = 0 or cl.id ilike :p_id_classe || '%')
+  (
+    SELECT
+      lp.cod_empresa,
+      em.nome AS empresa,
+      cl.id AS id_classe,
+      cl.nome AS classe,
+      lf.cod_pessoa AS cod_responsavel,
+      pe.nome_fantasia AS responsavel,
+      gr.id AS id_grupo,
+      gr.nome AS grupo,
+      lp.cod_produto,
+      pr.nome AS produto,
+      lp.preco,
+      pm.preco_medio,
+      lp.tipo_lancamento,
+      lp.data_movimento,
+      lp.quantidade,
+      CAST(lp.quantidade * pm.preco_medio AS numeric(14,2)) AS custo,
+      lp.valor,
+      CAST(lp.valor * (CASE WHEN total_valor_vendas <> 0 THEN (total_tributos_sobre_vendas / total_valor_vendas) ELSE 0 END) 
+      AS numeric(14,2)) AS tributos_sobre_venda
+    FROM
+      lancamentos_produtos lp
+      INNER JOIN produtos pr ON (lp.cod_produto = pr.cod_produto)
+      LEFT JOIN precos_medios pm ON (lp.cod_empresa = pm.cod_empresa AND lp.cod_produto = pm.cod_produto 
+        AND lp.data_movimento = pm.data_movimento AND COALESCE(lp.cod_deposito, 0) = COALESCE(pm.cod_deposito, 0)
+        AND lp.tipo_lancamento = pm.tipo_lancamento)
+      INNER JOIN grupos gr ON (pr.cod_grupo = gr.cod_grupo)
+      INNER JOIN lancamentos_financeiros lf ON (lp.cod_lanc_financeiro = lf.cod_lanc_financeiro)
+      INNER JOIN pessoas pe ON (lf.cod_pessoa = pe.cod_pessoa)
+      INNER JOIN classes cl ON (pe.cod_classe = cl.cod_classe)
+      INNER JOIN pessoas em ON (em.cod_pessoa = lp.cod_empresa)
+    WHERE
+      (COALESCE(0, lp.cod_empresa) = 0) AND
+      (COALESCE(0, lf.cod_pessoa) = 0) AND
+      lp.data_movimento BETWEEN '01/10/2024' AND current_date AND
+      lp.situacao = 2 AND
+      NOT lp.cancelado AND
+      lp.tipo_lancamento IN ('V', 'X')
   ) AS mov
-group by
+GROUP BY
   cod_empresa,
   empresa,
   id_classe,
@@ -106,9 +97,9 @@ group by
   preco,
   preco_medio,
   data_movimento
-order by
+ORDER BY
   cod_empresa,
   converte_id_para_comparar(id_classe),
   cod_responsavel,
   converte_id_para_comparar(id_grupo),
-  cod_produto
+  cod_produto;
